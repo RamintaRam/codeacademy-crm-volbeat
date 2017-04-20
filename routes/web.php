d71a68f26dc984aa5f20b5e442bc0c3e253b2c1c
RamintaRam <raminta5@yahoo.com>
2017-04-20 15:41:10 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,16 @@ use App\models\CrmPersons;
 
 Route::get('/', function () {
 
-CrmPersons::get();
 
-    return view('welcome');
+    return CrmPersons::get();
+});
+
+Route::get('/new-person', function ()
+{
+    return CrmPersons::create([
+        'id' => \Ramsey\Uuid\Uuid::uuid4(),
+        'name' => 'Raminta',
+        'email' => 'Bingelyte',
+        'phone' => '86xxxxxxx'
+    ]);
 });
