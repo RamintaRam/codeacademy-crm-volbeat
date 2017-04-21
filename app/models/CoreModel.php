541a8f7fa1153d72c43dfe54460e91aba9ddce01
RamintaRam <raminta5@yahoo.com>
2017-04-21 14:37:04 +0300

    Added Uuid function

--- app/models/CoreModel.php
+++ app/models/CoreModel.php
@@ -10,8 +10,18 @@ namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class CoreModel extends Model
 {
     use SoftDeletes;
+    public $incrementing = false;
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Uuid::uuid4();
+        });
+    }
 }
