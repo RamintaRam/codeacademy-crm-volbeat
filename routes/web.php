3b4fb0d8a8646aa63308a029dcf86000374bd7aa
RamintaRam <raminta5@yahoo.com>
2017-04-24 14:43:32 +0300

    Created routes for CRM tables

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/generate-fake-data/persons/{count}', [
 
 ]);
 
+
 Route:: get('/clients', [
     'uses' => 'CRMClientsController@index'
 ]);
@@ -21,4 +22,104 @@ Route::get('/generate-fake-data/clients/{count}', [
     'uses' => 'CRMFakeDataController@generateClients'
 
 ]);
+
+
+Route:: get('/projectsTypes', [
+    'uses' => 'CRMProjectsTypesController@index'
+]);
+
+Route::get('/generate-fake-data/projectsTypes/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsTypes'
+
+]);
+
+
+Route:: get('/projects', [
+    'uses' => 'CRMProjectsController@index'
+]);
+
+Route::get('/generate-fake-data/projects/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjects'
+
+
+]);
+
+Route:: get('/clientsPositions', [
+    'uses' => 'CRMClientsPositionsController@index'
+]);
+
+Route::get('/generate-fake-data/clientsPositions/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateClientsPositions'
+
+]);
+
+
+Route:: get('/projectsLoginsTypes', [
+    'uses' => 'CRMProjectsLoginsTypesController@index'
+]);
+
+Route::get('/generate-fake-data/projectsLoginsTypes/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsLoginsTypes'
+
+]);
+
+
+Route:: get('/projectsLogins', [
+    'uses' => 'CRMProjectsLoginsController@index'
+]);
+
+Route::get('/generate-fake-data/projectsLogins/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsLogins'
+
+]);
+
+
+Route:: get('/clientsPersonsPositionsConnections', [
+    'uses' => 'CRMClientsPersonsPositionsConnections@index'
+]);
+
+Route::get('/generate-fake-data/clientsPersonsPositionsConnections/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateClientsPersonsPositionConnection'
+
+]);
+
+
+Route:: get('/projectsLoginsConnections', [
+    'uses' => 'CRMProjectsLoginsConnections@index'
+]);
+
+Route::get('/generate-fake-data/projectsLoginsConnections/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsLoginsConnection'
+
+]);
+
+
+Route:: get('/projectsPersonsTypes', [
+    'uses' => 'CRMProjectsPersonsTypes@index'
+]);
+
+Route::get('/generate-fake-data/projectsPersonsTypes/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsPersonsTypes'
+
+]);
+
+
+Route:: get('/projectsPersonsTypesConnections', [
+    'uses' => 'CRMProjectsPersonsTypesConnections@index'
+]);
+
+Route::get('/generate-fake-data/projectsPersonsTypesConnections/{count}', [
+
+    'uses' => 'CRMFakeDataController@generateProjectsPersonsTypesConnections'
+
+]);
+
 
