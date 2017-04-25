4a5b0f119a56ea97d71cf423cd835b88317c919f
RamintaRam <raminta5@yahoo.com>
2017-04-25 15:13:34 +0300

    added more data

--- app/models/CRMClientsPersonsPositionsConnections.php
+++ app/models/CRMClientsPersonsPositionsConnections.php
@@ -12,5 +12,15 @@ class CRMClientsPersonsPositionsConnections extends CoreModel
     protected $table = 'crm_clients_persons_positions_connections';
 
     protected $fillable = ['id', 'clients_id', 'persons_id', 'position_id', 'comment'];
+
+    public function client ()
+    {
+        return $this->hasOne(CRMClients::class, 'id', 'clients_id');
+    }
+
+    public function people()
+    {
+        return $this->hasMany(CRMPersons::class, 'id', 'persons_id');
+    }
 }
 
